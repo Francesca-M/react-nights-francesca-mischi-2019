2a7098e5aa13b5b0b1b8eca599228fc8591db215
Francesca Mischi <francesca.mischi@gmail.com>
2019-03-24 19:29:02 +0100

    Added prettier and editorconfig

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import config from './config'
 
-const getToken = () => fetch(`${config.apiUrl}/oauth/token`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    grant_type: 'client_credentials',
-    client_id: config.clientId,
-    scope: config.scope
-  })
-}).then(res => res.json());
+const getToken = () =>
+  fetch(`${config.apiUrl}/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      grant_type: 'client_credentials',
+      client_id: config.clientId,
+      scope: config.scope,
+    }),
+  }).then(res => res.json())
 
 const getSkus = access_token =>
   fetch(`${config.apiUrl}/api/skus`, {
     headers: {
       'Content-type': 'application/json',
-      Authorization: `Bearer ${access_token}`
-    }
-  }).then(res => res.json());
+      Authorization: `Bearer ${access_token}`,
+    },
+  }).then(res => res.json())
 
 class App extends Component {
   state = {
     isLoading: true,
-    products: []
+    products: [],
   }
 
   async componentDidMount() {
-    const { access_token } = await getToken();
-    const products = await getSkus(access_token);
+    const { access_token } = await getToken()
+    const products = await getSkus(access_token)
 
-    this.setState({ products, isLoading: false });
+    this.setState({ products, isLoading: false })
   }
 
   render() {
     const {
       isLoading,
-      products: { data }
-    } = this.state;
+      products: { data },
+    } = this.state
 
     return (
       <div>
         <h1>Hi guys, thanks for checking my homework! :)</h1>
         {isLoading && '...'}
-        {!isLoading && <ul>
-          {data.map(item =>
-            <li key={item.id}>
-              <img src={item.attributes.image_url} width="50" alt=""></img>
-              <h2>{item.attributes.name}</h2>
-            </li>)}
-        </ul>
-        }
+        {!isLoading && (
+          <ul>
+            {data.map(item => (
+              <li key={item.id}>
+                <img src={item.attributes.image_url} width="50" alt="" />
+                <h2>{item.attributes.name}</h2>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    clientId: 'd1d19aba7f99661651d8ec73dcf4ba7979b3e98fff16c695a05a99f1291fc3a1',
-    scope: 'market:484',
-    apiUrl: 'https://reactnights2019.commercelayer.io'
+  clientId: 'd1d19aba7f99661651d8ec73dcf4ba7979b3e98fff16c695a05a99f1291fc3a1',
+  scope: 'market:484',
+  apiUrl: 'https://reactnights2019.commercelayer.io',
 }
